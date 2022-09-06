44dea9be9bf583dc22a3f5ca3fc2fee925dc0b04
dnfebri <dwinofebri@gmail.com>
2022-09-06 20:58:39 +0700

    done form free trial

--- src/components/InputText.js
+++ src/components/InputText.js
@@ -24,7 +24,7 @@ function InputText(props) {
   }
 
   return (
-    <div className="my-4">
+    <div className="pt-4">
       <label className="relative">
         <span className="absolute mx-2 top-0 left-0 opacity-70 transition-all duration-300">{props.label}</span>
         <input type={props.type} value={props.value} name={props.name} onChange={props.onChange} id={props.id} className="w-full bg-white outline-none border border-blue-primary h-10 px-2 group-focus:bg-black" onLoad={inputLoad} onFocus={inputForm} onBlur={outInput}/>
--- src/views/FormFreeTrial.js
+++ src/views/FormFreeTrial.js
@@ -19,10 +19,6 @@ function FormFreeTrial() {
     setInputs(values => ({...values, [name]: value}))
   }
 
-  function login(el) {
-    el.preventDefault()
-    console.log(inputs);
-  }
   function singup(el) {
     el.preventDefault()
     console.log(inputs);
@@ -38,25 +34,25 @@ function FormFreeTrial() {
 
   return (
     <div>
-      <div className="w-full h-screen" style={bgPage}>
-        <div className="flex justify-center my-8 py-8">
+      <div className="w-full py-8 md:grid md:grid-cols-2 min-h-screen" style={bgPage}>
+        <div className="grid justify-center items-center my-8">
           <div className="bg-red-400 relative">
-            <img src="/image/trial_img.jpg" alt="Free Trial" className="w-52" />
+            <img src="/image/trial_img.jpg" alt="Free Trial" className="w-52 md:w-full md:max-w-sm" />
             <div className="border-2 border-yellow-primary absolute top-3 -bottom-3 left-3 -right-3"></div>
             <div className="absolute -right-10 bottom-5 backdrop-blur-sm bg-white/30 rounded-xl pl-5 pr-10 text-yellow-primary font-judul text-xl">
               <h2>LET’S TRY THE <br/>CLUB!</h2>
             </div>
           </div>
         </div>
-        <div className="container">
-          <div className="bg-white p-6 text-blue-primary">
+        <div className="container md:grid md:justify-center">
+          <div className="bg-white p-6 text-blue-primary md:max-w-md">
             <h2 className="text-2xl">REDEEM FREE TRIAL ANDA!</h2>
             <div>
               <form onSubmit={singup}>
                 <div className="my-4">
                   <label>
-                    <span className="mx-2">Pilih Kota</span>
-                    <select className="block w-full my-2 py-1 px-2 bg-white border border-blue-primary">
+                    <span className="mx-2 text-xs">Pilih Kota</span>
+                    <select className="block w-full py-1 px-2 bg-white border border-blue-primary">
                       <option value="" key="">Pilih Kota</option>
                       <option value="Surabaya" key="1">Surabaya</option>
                       <option value="Jakarta" key="2">Jakarta</option>
@@ -65,8 +61,8 @@ function FormFreeTrial() {
                 </div>
                 <div className="my-4">
                   <label>
-                    <span className="mx-2">Pilih Club</span>
-                    <select className="block w-full my-2 py-1 px-2 bg-white border border-blue-primary">
+                    <span className="mx-2 text-xs">Pilih Club</span>
+                    <select className="block w-full py-1 px-2 bg-white border border-blue-primary">
                       <option value="" key="">Pilih Club</option>
                       <option value="Club A" key="1">Club A</option>
                       <option value="Club B" key="2">Club B</option>
@@ -75,41 +71,50 @@ function FormFreeTrial() {
                 </div>
                 <div className="py-0.5 flex items-end space-x-4 w-full">
                   <label className="my-4">
-                    <span className="mx-2">Gender</span>
-                    <select className="block w-full my-0 py-1.5 px-2 bg-white border border-blue-primary">
+                    <span className="mx-2 text-xs">Gender</span>
+                    <select className="block w-full py-1.5 px-2 bg-white border border-blue-primary">
                       <option value="" key="">Gender</option>
                       <option value="TUAN" key="1">TUAN</option>
                       <option value="NYOYA" key="2">NYOYA</option>
                     </select>
                   </label> 
-                  <div className="w-full">
+                  <div className="w-full my-3.5">
                     <InputText label="Your Name" type="name" name="name" id="name"
                     value={inputs.name || ''}
                     onChange={handleChange}
                     />
                   </div>
                 </div>
-                <div className="py-0.5">
+                <div className="my-4">
                   <InputText label="Phone" type="phone" name="phone" id="phone"
                   value={inputs.phone || ''} 
                   onChange={handleChange}
                   />
                 </div>
-                <div className="py-0.5">
+                <div className="my-4">
                   <InputText label="Password" type="password" name="password" 
                     value={inputs.password || ''} 
                     onChange={handleChange} 
                   />
                 </div>
-                <div className="py-1 flex items-center">
-                  {/* <input type="checkbox" value="true" className="w-4 h-4"/>
-                  <label className="text-xs pl-2">I’VE READ AND AGREE TO DATA PRIVACY POLICY.</label> */}
-                </div>
                 <div className="my-4">
-                  <button type='submit' className="border border-blue-primary py-1 w-full hover:bg-yellow-primary transition-all duration-300">Sing Up</button>
+                  <label>
+                    <span className="mx-2 text-xs">PREFERED CONTACT TIME</span>
+                    <select className="block w-full py-1 px-2 bg-white border border-blue-primary">
+                      <option value="" key="">Pilih Club</option>
+                      <option value="ANYTIME" key="1">ANYTIME</option>
+                      <option value="MORNING" key="2">MORNING</option>
+                      <option value="AFTERNOON" key="3">AFTERNOON</option>
+                      <option value="NIGHT" key="4">NIGHT</option>
+                    </select>
+                  </label> 
                 </div>
-                <div className="mt-8 text-sm flex space-x-2">
-                  <p>Don’t have an account ? </p><button onClick={changeForm}>Login</button>
+                <div className="mb-4 py-1 flex items-center">
+                  <input type="checkbox" value="true" className="w-4 h-4"/>
+                  <label className="text-xs pl-2">I’VE READ AND AGREE TO DATA PRIVACY POLICY.</label>
+                </div>
+                <div className="py-4">
+                  <button type='submit' className="border border-blue-primary py-1 w-full hover:bg-yellow-primary transition-all duration-300">REDEEM</button>
                 </div>
               </form>
             </div>
